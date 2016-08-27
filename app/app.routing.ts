c9e14c852b7fda08d05ac650627f3a7790d92a06
Zakhar Gulchak <z.gulchak@levi9.com>
2016-08-27 15:13:03 +0300

    Added routes, custom styles

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -1,10 +1,12 @@
 import {Routes, RouterModule} from '@angular/router';
 
+import {SearchResultsComponent} from './components/search.results.component';
 import {WelcomeComponent} from './components/welcome.component';
 
 export const appRoutes: Routes = [
     {path: '', redirectTo: '/welcome', pathMatch: 'full'},
-    {path: 'welcome', component: WelcomeComponent}
+    {path: 'welcome', component: WelcomeComponent},
+    {path: 'search', component: SearchResultsComponent}
 ];
 
 export const routing = RouterModule.forRoot(appRoutes);
--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: 'my-app',
+    selector: 'musify-app',
     templateUrl: '/app/components/app.component.html'
 })
 export class AppComponent implements OnInit {
-    title = 'Search your favourite music'; // todo how to set this title into html
+    title = '\uD83C\uDFB5 Musify \uD83C\uDFB5 Search your favourite music'; // todo how to set this title into html
 
     ngOnInit() {
     }
